7a5bf1856b58f1b48964d534115b0989eeb2091d
satv2025 <traplegionrecords2020@gmail.com>
2025-05-12 19:37:06 -0300

    act

---
+++ assets/js/plyr/locale/es.js
@@ -0,0 +1,24 @@
+const player = new Plyr('#player', {
+  i18n: {
+    // Traducciones personalizadas
+    'play': 'Reproducir',
+    'pause': 'Pausa',
+    'rewind': 'Rebobinar',
+    'fast_forward': 'Avanzar rápido',
+    'current_time': 'Tiempo actual',
+    'duration': 'Duración',
+    'mute': 'Silenciar',
+    'unmute': 'Activar sonido',
+    'volume': 'Volumen',
+    'fullscreen': 'Pantalla completa',
+    'exit_fullscreen': 'Salir de pantalla completa',
+    'captions': 'Subtítulos',
+    'settings': 'Configuración',
+    'download': 'Descargar',
+    'quality': 'Calidad',
+    'speed': 'Velocidad',
+    'reset': 'Restablecer',
+    'subtitles': 'Subtítulos',
+    'quality': 'Calidad',
+  }
+});
