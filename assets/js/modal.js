225aa63422c01dd2c7f0f5e12275f4ede7b8df6e
satv2025 <traplegionrecords2020@gmail.com>
2025-09-09 12:51:34 -0300

    ACT

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -394,7 +394,7 @@ function changeSeason(season, movieKey) {
                     <h3>${ep.title}</h3>
                     <p>${ep.description}</p>
                     <span>${ep.duration}</span>
-                    <div>${ep.number || ""}</div>
+                    <div class="episode-number">${ep.number || ""}</div>
                 </div>`;
             episodeList.appendChild(li);
         });
